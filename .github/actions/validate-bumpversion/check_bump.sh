bbfc9c43ce80557a799bdf0119e741fa12f5af46
Akseli Lehtniemi <60463103+Akaseli@users.noreply.github.com>
2026-07-02 09:47:01 +0300

    fix: path + warn about not finding file

--- .github/actions/validate-bumpversion/check_bump.sh
+++ .github/actions/validate-bumpversion/check_bump.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-ORIGIN_REF="origin/$BASE_REF:.bumpversion.toml"
+ORIGIN_REF="origin/$BASE_REF:./.bumpversion.toml"
 CURRENT=$(bump-my-version show current_version --config-file .bumpversion.toml)
 git fetch origin "$BASE_REF" --depth=1
 
 if ! git show "$ORIGIN_REF"  > /dev/null; then
-  echo "No .bumpversion.toml found in origin/$BASE_REF:$VERSION_PATH — treating as new file, skipping version check"
+  echo "::warning::No .bumpversion.toml found in origin/$BASE_REF:$VERSION_PATH — treating as new file, skipping version check"
   echo "Current version: $CURRENT"
   exit 0
 fi
